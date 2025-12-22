cf88c107f211be8d1d87d939271580d98049fc61
Mohsen mohammadi <mohsen.mohammadii3446@gmail.com>
2025-12-23 00:56:19 +0330

    initializ project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import './index.css'
+// import { Button } from "@/components/ui/button"
+// import { Card,CardHeader } from "@/components/ui/card"
+
+
+function App() {
+return(
+  <div className="App">
+    <h1>Hello AsanDaroo</h1>
+  </div>
+)
+}
+
+export  default App
